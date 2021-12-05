0ff5c7bf8e08a1ec2099da7b4a5c6f03b4b20be9
TaeBlessK <ktg3151@gmail.com>
2021-12-05 23:10:15 +0900

    background fixed

--- Escape_test/Assets/Script/Background.cs
+++ Escape_test/Assets/Script/Background.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
+    public GameObject BG; //생성할 배경
     private float width;
-    private float height;
+    private float Rewidth;
 
     public float speed = 3f;
 
     private void Awake()
     {
         BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
-        width = backgroundCollider.size.x;
-        height = backgroundCollider.size.y;
+        width = backgroundCollider.size.x; //x 길이 = width
+        Rewidth = backgroundCollider.transform.position.x;
     }
 
 
     void Update()
     {
         ScrollingObject();
-        if (transform.position.x <= -width)  Reposition();
+
+        if (transform.position.x <= -width*2f)  Reposition();
 
     }
 
     private void Reposition()
     {
   
-        Vector2 offset = new Vector2(width * 2, 0);
+        Vector2 offset = new Vector2(width*3f, 0);
         transform.position = (Vector2)transform.position + offset;
     }
 
     private void ScrollingObject()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        transform.Translate(Vector3.left * speed * Time.deltaTime); //좌측으로 이동
     }
+
 }
