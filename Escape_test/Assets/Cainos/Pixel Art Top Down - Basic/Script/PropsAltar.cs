95ea247e13e1730373dff3ee94d72c4c4bd50acb
TaeBlessK <ktg3151@gmail.com>
2021-12-09 03:18:09 +0900

    add potal

---
+++ Escape_test/Assets/Cainos/Pixel Art Top Down - Basic/Script/PropsAltar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//when something get into the alta, make the runes glow
+namespace Cainos.PixelArtTopDown_Basic
+{
+
+    public class PropsAltar : MonoBehaviour
+    {
+        public List<SpriteRenderer> runes;
+        public float lerpSpeed;
+
+        private Color curColor;
+        private Color targetColor;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                targetColor = new Color(1, 1, 1, 1);
+            }
+
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                targetColor = new Color(255, 255, 1, 0);
+            }
+
+        }
+
+        private void Update()
+        {
+            curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
+
+            foreach (var r in runes)
+            {
+                Debug.Log(curColor);
+                r.color = curColor;
+            }
+        }
+    }
+}
